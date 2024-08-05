84130111995fa05d30f0df871988bd7fbeffed93
Sheharyar-1 <sheharyar.ahmed@xprolabs.com>
2024-08-05 19:02:12 +0500

    Override devise password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+
+
+  validates :password, length: { maximum: 10 }
+
+  validate :password_special_character
+
+  private
+
+  def password_special_character
+    return if password.blank?
+
+    unless password.match?(/[^a-zA-Z0-9]/)
+      errors.add :password, "must include at least one special character"
+    end
+  end
 end
