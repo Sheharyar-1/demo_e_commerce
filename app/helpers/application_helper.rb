8182cda1ebd7b7813f4518581bdf7dbeee79f661
Sheharyar-1 <sheharyar.ahmed@xprolabs.com>
2024-08-19 18:27:52 +0500

    Orders list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
     if session[:order_id]
       order = current_user.order.find_by(id: session[:order_id])
       if order.nil? || order.placed?
-        order = current_user.order.create
+        order = current_user.order.new
         session[:order_id] = order.id
       end
     else
-      order = current_user.order.create
+      order = current_user.order.new
       session[:order_id] = order.id
     end
     order
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
+  get 'orders/index'
   get 'products/index'
   root 'products#index'
   get 'user/list', to: 'user#list', as: 'list_user'
   patch 'carts/placed', to: 'carts#placed', as: 'placed_order'
   get 'carts/address', to: 'carts#address', as: 'order_address'
 
-
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resources :user, except: [:show]
   resources :order_items
   resource :carts, only: [:show]
-
+  resources :orders, only: [:index]
   resources :products do 
     resources :multi_step
   end
